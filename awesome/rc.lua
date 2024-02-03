9b53040ef92ba149ebd75f7872d2dd8eab45314f
Ewheeler121 <ewheeler121@outlook.com>
2024-02-02 23:51:58 -0500

    no longer need udiskie to run on window manager

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -270,7 +270,6 @@ local function run_once(cmd_arr)
         for _, cmd in ipairs(cmd_arr) do
             awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
         end
-            awful.spawn.with_shell("udiskie &")
             awful.spawn.with_shell("picom --daemon &")
             awful.spawn.with_shell("steam &")
             awful.spawn.with_shell("discord &")
